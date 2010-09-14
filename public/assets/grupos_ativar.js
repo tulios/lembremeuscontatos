e2556b5daaec36053cb519e1d726e4da1854e181
tulios <ornelas.tulio@gmail.com>
2010-09-13 21:24:18 -0300

    assets at Monday 13, 2010

---
+++ public/assets/grupos_ativar.js
@@ -0,0 +1,1 @@
+jQuery(document).ready(function(){$("a#ativar").fancybox({autoScale:false,overlayOpacity:0.7,autoDimensions:true,onComplete:function(){}});aplicarBloquearBotaoESubmeterForm()});function aplicarBloquearBotaoESubmeterForm(){$("#submit_grupo").click(function(a){a.preventDefault();bloquearBotao($(this),"Ativando...");$("#ativar_form").submit()})};
