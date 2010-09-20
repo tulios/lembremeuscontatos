d0bc2b49c8716b0eb49a58a0008e9d6a8e3552b0
tulios <ornelas.tulio@gmail.com>
2010-09-19 23:30:29 -0300

    delete assets at Sunday 19, 2010

--- public/assets/grupos_ativar.js
+++
@@ -1,1 +0,0 @@
-jQuery(document).ready(function(){$("a#ativar").fancybox({autoScale:false,overlayOpacity:0.7,autoDimensions:true,onComplete:function(){}});aplicarBloquearBotaoESubmeterForm()});function aplicarBloquearBotaoESubmeterForm(){$("#submit_grupo").click(function(a){a.preventDefault();bloquearBotao($(this),"Ativando...");$("#ativar_form").submit()})};
