42d6688f7daaced81714cc588a9271946de4a56c
tulios <ornelas.tulio@gmail.com>
2010-09-23 21:36:03 -0300

    delete assets at Thursday 23, 2010

--- public/assets/grupos_desativar.js
+++
@@ -1,1 +0,0 @@
-jQuery(document).ready(function(){$("a#desativar").fancybox({autoScale:false,overlayOpacity:0.7,autoDimensions:true,onComplete:function(){}});aplicarBloquearBotaoESubmeterForm()});function aplicarBloquearBotaoESubmeterForm(){$("#submit_grupo").click(function(a){a.preventDefault();bloquearBotao($(this),"Desativando...");$("#desativar_form").submit()})};
